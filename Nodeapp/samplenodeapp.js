45c39201f5ee2460cf9353bb84ea7dd4ca0f3b1c
sivakumar193654 <suravarapukumar07@gmail.com>
2022-09-06 10:28:21 +0530

    updated

--- Nodeapp/samplenodeapp.js
+++ Nodeapp/samplenodeapp.js
@@ -1,13 +1,14 @@
-var express = require('express')
-var app = express()
+'use strict';
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+const express = require('express')
+const app = express();
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
+const port = 8080;
+const host = '0.0.0.0';
+
+app.get('/', (req, res) => {
+  res.send('Hello World from IBM Cloud Essentials!');
 })
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.listen(port, host);
+console.log(`Running on http://${host}:${port}`);
